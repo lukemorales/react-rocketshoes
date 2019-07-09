9983a618410fd05b019298d9987a99bdc5f81747
Luke Morales <lukemorales@live.com>
2019-07-09 20:34:28 -0300

    chore: Refactured Actions to Saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,12 +1,22 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
+import { formatPrice } from '../../../utils/format';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `products/${id}`);
+  const productExists = yield select(state => state.cart.find(p => (p.id = id)));
 
-  yield put(addToCartSuccess(response.data));
+  if (productExists) {
+    const { amount } = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `products/${id}`);
+
+    const data = { ...response.data, amount: 1, priceFormatted: formatPrice(response.data.price) };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
