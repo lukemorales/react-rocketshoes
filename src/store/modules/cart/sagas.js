252c191bd11994bbb94115f08a9f32e1a2676bc0
Luke Morales <lukemorales@live.com>
2019-07-09 20:42:48 -0300

    feat: Verify Product Stock Before Adding to Cart

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -5,10 +5,21 @@ import { addToCartSuccess, updateAmount } from './actions';
 import { formatPrice } from '../../../utils/format';
 
 function* addToCart({ id }) {
-  const productExists = yield select(state => state.cart.find(p => (p.id = id)));
+  const productExists = yield select(state => state.cart.find(p => p.id === id));
+
+  const stock = yield call(api.get, `stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO');
+    return;
+  }
 
   if (productExists) {
-    const { amount } = productExists.amount + 1;
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `products/${id}`);
