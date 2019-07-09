2e131a9cb7a4de1633847cfc877b0fc8dbc6b68b
Luke Morales <lukemorales@live.com>
2019-07-09 17:21:17 -0300

    chore: Integrated Main page with API

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,111 +1,45 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
 import { ProductList } from './Home_Styles';
 
 export default class Home extends Component {
   state = {
-    newState: null,
+    products: [],
   };
 
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
   render() {
+    const { products } = this.state;
+
     return (
       <ProductList>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
-            alt="Tenis"
-          />
-          <strong>Tênis daí</strong>
-          <span>R$ 129,90</span>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
 
-            <span>ADD TO CART</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
-            alt=""
-          />
-          <strong>Tênis daí</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-
-            <span>ADD TO CART</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
-            alt=""
-          />
-          <strong>Tênis daí</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-
-            <span>ADD TO CART</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
-            alt=""
-          />
-          <strong>Tênis daí</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-
-            <span>ADD TO CART</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
-            alt=""
-          />
-          <strong>Tênis daí</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-
-            <span>ADD TO CART</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
-            alt=""
-          />
-          <strong>Tênis daí</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-
-            <span>ADD TO CART</span>
-          </button>
-        </li>
+              <span>ADD TO CART</span>
+            </button>
+          </li>
+        ))}
       </ProductList>
     );
   }
