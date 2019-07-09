f0403513499c174c853619fe7a220c55607377a5
Luke Morales <lukemorales@live.com>
2019-07-09 17:01:04 -0300

    chore: Structured Cart Page

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import {} from './Cart_Styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+import { Container, ProductTable, Total } from './Cart_Styles';
 
 export default class Cart extends Component {
   state = {
@@ -8,9 +9,59 @@ export default class Cart extends Component {
 
   render() {
     return (
-      <>
-        <h1>Cart Component</h1>
-      </>
+      <Container>
+        <ProductTable>
+          <thead>
+            <tr>
+              <th />
+              <th>PRODUTO</th>
+              <th>QTD</th>
+              <th>SUBTOTAL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img
+                  src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
+                  alt=""
+                />
+              </td>
+              <td>
+                <strong>Tênis muito massa</strong>
+                <span>R$ 129,90</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={2} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$ 258.8</strong>
+              </td>
+              <td>
+                <button type="button">
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </ProductTable>
+
+        <footer>
+          <button type="submit">Purchase</button>
+          <Total>
+            <span>TOTAL:</span>
+            <strong>R$ 1920,28</strong>
+          </Total>
+        </footer>
+      </Container>
     );
   }
 }
---
+++ src/pages/Cart/Cart_Styles.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
+
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      background: #7159c1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background 180ms ease-in-out;
+
+      &:hover {
+        background: ${darken(0.04, '#7159c1')};
+      }
+    }
+  }
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+
+  thead th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
+  }
+
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+  }
+
+  img {
+    height: 100px;
+  }
+
+  strong {
+    display: block;
+  }
+
+  span {
+    display: block;
+    margin-top: 2px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 3px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+      text-align: center;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`;
+
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #999;
+    font-weight: bold;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 8px;
+  }
+`;
