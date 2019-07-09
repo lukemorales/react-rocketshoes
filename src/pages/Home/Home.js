b2d69252ec1c34978edf8b996c69e0f29f578d5e
Luke Morales <lukemorales@live.com>
2019-07-09 16:37:51 -0300

    chore: Structured Main page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import {} from './Home_Styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './Home_Styles';
 
 export default class Home extends Component {
   state = {
@@ -8,9 +9,104 @@ export default class Home extends Component {
 
   render() {
     return (
-      <>
-        <h1>Home Component</h1>
-      </>
+      <ProductList>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
+            alt="Tenis"
+          />
+          <strong>Tênis daí</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
+            alt=""
+          />
+          <strong>Tênis daí</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
+            alt=""
+          />
+          <strong>Tênis daí</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
+            alt=""
+          />
+          <strong>Tênis daí</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
+            alt=""
+          />
+          <strong>Tênis daí</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-downshifter-8-feminino/26/D12-9470-026/D12-9470-026_detalhe1.jpg?resize=280:280"
+            alt=""
+          />
+          <strong>Tênis daí</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+      </ProductList>
     );
   }
 }
---
+++ src/pages/Home/Home_Styles.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const ProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style-type: none;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+
+    img {
+      align-self: center;
+      max-height: 250px;
+    }
+
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      background: #7159c1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      overflow: hidden;
+      margin-top: auto;
+      display: flex;
+      align-items: center;
+      transition: 180ms ease-in-out;
+
+      &:hover {
+        background: ${darken(0.04, '#7159c1')};
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        background: rgba(0, 0, 0, 0.1);
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+  }
+`;
