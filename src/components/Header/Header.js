ee753ccc7c5a8f56a684d7f6c040c7edc7072022
Luke Morales <lukemorales@live.com>
2019-07-09 22:12:56 -0300

    chore: Added Missing PropTypes to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
@@ -16,13 +17,21 @@ function Header({ cartSize }) {
       <Cart to="/cart">
         <div>
           <strong>My Cart</strong>
-          <span>{cartSize === 1 ? `${cartSize} item` : `${cartSize} items`}</span>
+          <span>{cartSize === 1 ? `${cartSize} product` : `${cartSize} products`}</span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
     </Container>
   );
 }
+
+Header.propTypes = {
+  cartSize: PropTypes.number,
+};
+
+Header.defaultProps = {
+  cartSize: 0,
+};
 
 export default connect(state => ({
   cartSize: state.cart.length,
