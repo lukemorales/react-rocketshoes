8124be6231b2e0eeac2d990434b66694bdb492b1
Luke Morales <lukemorales@live.com>
2019-07-12 12:08:39 -0300

    chore: Updated API baseURL to work on Netlify

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-json-server.typicode.com/lukemorales/api-rocketshoes',
 });
 
 export default api;
